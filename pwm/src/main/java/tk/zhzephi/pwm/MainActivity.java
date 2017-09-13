f187aacb15e4bd902e31f08b84f998671f450a06
zongfi <zhzephizhao@huangbaoche.com>
2017-09-13 21:23:53 +0800

    pwm控制测试

--- pwm/src/main/java/tk/zhzephi/pwm/MainActivity.java
+++ pwm/src/main/java/tk/zhzephi/pwm/MainActivity.java
@@ -15,9 +15,8 @@ public class MainActivity extends Activity {
 
     private static final String PWM_BUS = "PWM0";
     private Pwm pwm;
-    private double sum = 1;
-    private boolean mIsPulseIncreasing = true;
 
+    private double sum = 1;
     private Handler handler = new Handler();
 
     @Override
@@ -26,20 +25,20 @@ public class MainActivity extends Activity {
         PeripheralManagerService service = new PeripheralManagerService();
         try {
             pwm = service.openPwm(PWM_BUS);
-            pwm.setPwmFrequencyHz(50);
-            pwm.setPwmDutyCycle(1);
+            pwm.setPwmFrequencyHz(120); //周期120Hz(有效期8.33ms),责任(时间25%,每周期2.08ms)
+            pwm.setPwmDutyCycle(50);
             pwm.setEnabled(true);
         } catch (IOException e) {
             Log.e(TAG, "Error setting the angle", e);
         }
-        handler.postDelayed(runnable, 300);
+        handler.postDelayed(runnable, 2000);
     }
 
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
             setupServo();
-            handler.postDelayed(runnable, 300);
+            handler.postDelayed(runnable, 2000);
         }
     };
 
@@ -51,21 +50,15 @@ public class MainActivity extends Activity {
 
     private void setupServo() {
         try {
-            Log.d(TAG, "===GO====sum:" + sum);
-            if (mIsPulseIncreasing) {
-                sum += 0.2;
-            } else {
-                sum -= 0.2;
-            }
-            if (sum > 2) {
-                sum = 2;
-                mIsPulseIncreasing = !mIsPulseIncreasing;
-            } else if (sum < 1) {
-                sum = 1;
-                mIsPulseIncreasing = !mIsPulseIncreasing;
+            sum++;
+            if (sum > 100) {
+                sum = 0;
             }
             if (pwm != null) {
-                pwm.setPwmDutyCycle(100 * sum / 20);
+                Log.d(TAG, "Value sum:" + sum);
+//                double cycle = 100 * sum / hz;
+//                Log.d(TAG, "This is pwmDutyCycle value :" + cycle);
+                pwm.setPwmDutyCycle(sum);
             }
         } catch (IOException e) {
             Log.e(TAG, "Error creating Servo", e);
