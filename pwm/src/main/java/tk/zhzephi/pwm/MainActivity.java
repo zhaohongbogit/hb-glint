a5bbbb18b6660a0c6155ce508a7ded57c5415694
zongfi <zhzephizhao@huangbaoche.com>
2017-10-18 11:30:19 +0800

    pwm控制测试

--- pwm/src/main/java/tk/zhzephi/pwm/MainActivity.java
+++ pwm/src/main/java/tk/zhzephi/pwm/MainActivity.java
@@ -10,13 +10,28 @@ import com.google.android.things.pio.Pwm;
 
 import java.io.IOException;
 
+/**
+ * 本次实验使用的舵机为SG90（辉盛），需要注意的有一下几点
+ *  1. SG90脉冲周期为20ms，也就是Hz=50
+ *  2. SG90支持的旋转角度为180度，脉宽0.5ms-2.5ms对应的角度-90度到+90度，对应的占空比为2.5%-12.5%，所以
+ *  7.5%对应了舵机在中立点（0度）
+ */
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    // Parameters of the servo PWM
+    private static final double MIN_ACTIVE_PULSE_DURATION_MS = 1;
+    private static final double MAX_ACTIVE_PULSE_DURATION_MS = 2;
+    private static final double PULSE_PERIOD_MS = 20;  // Frequency of 50Hz (1000/20)
+
+    // Parameters for the servo movement over time
+    private static final double PULSE_CHANGE_PER_STEP_MS = 0.2;
+    private static final int INTERVAL_BETWEEN_STEPS_MS = 1000;
+
     private static final String PWM_BUS = "PWM0";
     private Pwm pwm;
-
-    private double sum = 1;
+    private double mActivePulseDuration;
+    private boolean mIsPulseIncreasing = true;
     private Handler handler = new Handler();
 
     @Override
@@ -24,21 +39,23 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         PeripheralManagerService service = new PeripheralManagerService();
         try {
+            mActivePulseDuration = MIN_ACTIVE_PULSE_DURATION_MS;
             pwm = service.openPwm(PWM_BUS);
-            pwm.setPwmFrequencyHz(120); //周期120Hz(有效期8.33ms),责任(时间25%,每周期2.08ms)
-            pwm.setPwmDutyCycle(50);
+            pwm.setPwmFrequencyHz(1000 / PULSE_PERIOD_MS); //周期50Hz(有效期20ms),责任(时间25%,每周期2.08ms)
+            pwm.setPwmDutyCycle(mActivePulseDuration);
             pwm.setEnabled(true);
         } catch (IOException e) {
             Log.e(TAG, "Error setting the angle", e);
         }
-        handler.postDelayed(runnable, 2000);
+        handler.post(runnable);
     }
 
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
             setupServo();
-            handler.postDelayed(runnable, 2000);
+            // Reschedule the same runnable in {@link #INTERVAL_BETWEEN_STEPS_MS} milliseconds
+            handler.postDelayed(runnable, INTERVAL_BETWEEN_STEPS_MS);
         }
     };
 
@@ -49,17 +66,34 @@ public class MainActivity extends Activity {
     }
 
     private void setupServo() {
+        if (pwm == null) {
+            return;
+        }
+
+        // Change the duration of the active PWM pulse, but keep it between the minimum and
+        // maximum limits.
+        // The direction of the change depends on the mIsPulseIncreasing variable, so the pulse
+        // will bounce from MIN to MAX.
+        if (mIsPulseIncreasing) {
+            mActivePulseDuration += PULSE_CHANGE_PER_STEP_MS;
+        } else {
+            mActivePulseDuration -= PULSE_CHANGE_PER_STEP_MS;
+        }
+
+        // Bounce mActivePulseDuration back from the limits
+        if (mActivePulseDuration > MAX_ACTIVE_PULSE_DURATION_MS) {
+            mActivePulseDuration = MAX_ACTIVE_PULSE_DURATION_MS;
+            mIsPulseIncreasing = !mIsPulseIncreasing;
+        } else if (mActivePulseDuration < MIN_ACTIVE_PULSE_DURATION_MS) {
+            mActivePulseDuration = MIN_ACTIVE_PULSE_DURATION_MS;
+            mIsPulseIncreasing = !mIsPulseIncreasing;
+        }
+        Log.d(TAG, "Changing PWM active pulse duration to " + mActivePulseDuration + " ms");
+
         try {
-            sum++;
-            if (sum > 100) {
-                sum = 0;
-            }
-            if (pwm != null) {
-                Log.d(TAG, "Value sum:" + sum);
-//                double cycle = 100 * sum / hz;
-//                Log.d(TAG, "This is pwmDutyCycle value :" + cycle);
-                pwm.setPwmDutyCycle(sum);
-            }
+            // Duty cycle is the percentage of active (on) pulse over the total duration of the
+            // PWM pulse
+            pwm.setPwmDutyCycle(100 * mActivePulseDuration / PULSE_PERIOD_MS);
         } catch (IOException e) {
             Log.e(TAG, "Error creating Servo", e);
         }
---
+++ pwm2/src/main/java/tk/zhzephi/pwm2/PwmEngine.java
@@ -0,0 +1,166 @@
+package tk.zhzephi.pwm2;
+
+import android.util.Log;
+
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.Pwm;
+
+import java.io.IOException;
+
+/**
+ * Pwm舵机控制引擎
+ * Created by HONGBO on 2017/10/17 16:45.
+ */
+
+public class PwmEngine {
+
+    private Pwm mPwm;
+
+    private final static int STATE_START = 1;
+    private final static int STATE_STOP = 2;
+    private int mState = STATE_STOP;
+    private Thread swingThread;
+    private static final int BEGIN_PLACE = 0;
+
+    public PwmEngine(String pwmName) {
+        PeripheralManagerService service = new PeripheralManagerService();
+        try {
+            mPwm = service.openPwm(pwmName);
+            mPwm.setPwmFrequencyHz(50);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 控制舵机转到对应角度，degree在[0,180]区间内
+     *
+     * @param degree
+     */
+    public void setDegree(double degree) {
+        if (degree < 0 || degree > 180) {
+            return;
+        }
+        try {
+            mPwm.setEnabled(true);
+//            int i = 18;
+//            while (i > 0) {
+//                i--;
+            mPwm.setPwmDutyCycle(2.5 + 10 * degree / 180);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+//            }
+            mPwm.setEnabled(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 控制舵机转到对应角度，degree在[0,180]区间内
+     * gap用来控制完成转动的时间，也就是控制转动的速度。
+     * gap的单位是毫秒，ms。
+     * 一次偏转需要多个脉冲周期，每个转动的脉冲信号后，gap的时间间隔里会发出平信号。
+     *
+     * @param degree
+     * @param gap
+     */
+    public void setDegree(double degree, int gap) {
+        if (degree < 0 || degree > 180) {
+            return;
+        }
+        try {
+            mPwm.setEnabled(true);
+            int i = 18; //设置18个脉冲周期确保角度转到位置，经测试最少17个周期才能到位(转动比较快，一周期机械运动不一定到位)
+            while (i > 0) {
+                i--;
+                try {
+                    mPwm.setPwmDutyCycle(2.5 + 10 * degree / 180);
+                    Thread.sleep(20);
+                    mPwm.setPwmDutyCycle(0);
+                    Thread.sleep(gap);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            mPwm.setEnabled(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 控制舵机在begin和end之间摆动.
+     * gapTime控制摆动的速度，最快的速度是gapTime =0；gapTime是每次脉冲信号后，发出平信号的时间间隔。
+     * 会创建新的线程执行。
+     *
+     * @param begin
+     * @param end
+     * @param gapTime
+     */
+    public void startSwing(final double begin, final double end, final int gapTime) {
+        if (begin < 0 || begin > 180 || end < 0 || end > 180 || gapTime < 0) {
+            return;
+        }
+        if (STATE_STOP == mState) {
+            mState = STATE_START;
+            swingThread = new Thread() {
+                @Override
+                public void run() {
+                    swing(begin, end, gapTime);
+                }
+            };
+            if (null != swingThread) {
+                swingThread.start();
+            }
+        }
+    }
+
+    private void swing(double begin, double end, int gapTime) {
+        if (STATE_START == mState)
+            setDegree(BEGIN_PLACE);
+        while (STATE_START == mState) {
+            setDegree(begin, gapTime);
+            setDegree(end, gapTime);
+        }
+    }
+
+    /**
+     * 停止摆动，回收资源
+     */
+    public void stopSwing() {
+        if (STATE_START == mState) {
+            mState = STATE_STOP;
+            Log.d("---->", "stop start");
+            if (null != swingThread) {
+                try {
+                    Log.d("---->", "wait recognition thread exit");
+                    swingThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    swingThread = null;
+                }
+            }
+            Log.d("---->", "stop end");
+        }
+    }
+
+    /**
+     * 关闭PWM端口连接
+     */
+    public void close() {
+        if (mPwm != null) {
+            try {
+                mPwm.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                mPwm = null;
+            }
+        }
+    }
+}
