47c5be82ce77e5b7784bb5e3b357e413f9a3d11c
Hongbo <zhzephizhao@huangbaoche.com>
2018-12-08 12:38:37 +0800

    增加线路图文件

--- switch/src/main/java/tk/hongbo/aswitch/MainActivity.java
+++ switch/src/main/java/tk/hongbo/aswitch/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.things.contrib.driver.button.Button;
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManager;
 
 import java.io.IOException;
 
@@ -12,12 +14,20 @@ public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private static final String gpioButtonPinName = "GPIO2_IO00";
+    private static final String gpioLedPinName = "GPIO2_IO05";
     private Button mButton;
-
+    private Gpio led;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PeripheralManager peripheralManager = PeripheralManager.getInstance();
+        try {
+            led = peripheralManager.openGpio(gpioLedPinName);
+            led.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException e) {
+            Log.d(TAG, "Init led error", e);
+        }
         setupButton();
     }
 
@@ -25,24 +35,33 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         destroyButton();
+        if (led != null) {
+            try {
+                led.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Close led error", e);
+            } finally {
+                led = null;
+            }
+        }
     }
 
     private void setupButton() {
         try {
-            mButton = new Button(gpioButtonPinName,
-                    // high signal indicates the button is pressed
-                    // use with a pull-down resistor
-                    Button.LogicState.PRESSED_WHEN_HIGH
-            );
+            mButton = new Button(gpioButtonPinName, Button.LogicState.PRESSED_WHEN_LOW);
             mButton.setOnButtonEventListener(new Button.OnButtonEventListener() {
                 @Override
                 public void onButtonEvent(Button button, boolean pressed) {
-                    // do something awesome
                     Log.d(TAG, "按钮状态：" + pressed);
+                    try {
+                        led.setValue(pressed);
+                    } catch (IOException e) {
+                        Log.e(TAG, "Led set value error", e);
+                    }
                 }
             });
         } catch (IOException e) {
-            // couldn't configure the button...
+            Log.e(TAG, "初始化按钮失败", e);
         }
     }
 
